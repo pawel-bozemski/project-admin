c9333702146c29a6c4056f9f1dcb8046cb2ade02
Pawel Bozemski <paw.boz@gmail.com>
2019-12-03 18:46:34 +0100

    Add mobile menu and js function to activate it

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+// add class active to mobile wrapper after clicking on hamburger icon
 
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.querySelector('.navigation__wrapper-mobile');
+
+hamburger.addEventListener('click', function(){
+    mobileMenu.classList.toggle('active');
+});
